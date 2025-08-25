ec01372a03eca0b3c6f8c99f224e70d85c1a01c5
Ariel Piñeiro <ariel.pieiro@iskaypet.com>
2025-08-25 12:19:28 -0300

    bumps

--- otelfiber/semconv.go
+++ otelfiber/semconv.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/utils"
 	"go.opentelemetry.io/otel/attribute"
-	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
 )
 
 var (
@@ -50,12 +50,12 @@ func httpServerTraceAttributesFromRequest(c *fiber.Ctx, cfg config) []attribute.
 		semconv.URLFull(utils.CopyString(c.OriginalURL())),
 		semconv.UserAgentOriginal(string(utils.CopyBytes(c.Request().Header.UserAgent()))),
 		semconv.ServerAddress(utils.CopyString(c.Hostname())),
-		semconv.NetTransportTCP,
+		semconv.NetworkTransportTCP,
 	}
 	attrs = append(attrs, protocolAttributes...)
 
 	if cfg.Port != nil {
-		attrs = append(attrs, semconv.NetHostPortKey.Int(*cfg.Port))
+		attrs = append(attrs, semconv.ServerPort(*cfg.Port))
 	}
 
 	if username, ok := HasBasicAuth(c.Get(fiber.HeaderAuthorization)); ok {
