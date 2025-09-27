a2f9625775ff62d1f9114eb8164ceb1c0a95bf53
ariel.pineiro <ariel.pineiro@iskaypet.com>
2025-09-27 10:22:18 +0200

    bumps

---
+++ upgrade.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT_DIR=$(pwd)
+
+# Buscar todos los go.mod y recorrerlos
+for gomod_file in $(find . -name "go.mod"); do
+  dir=$(dirname "$gomod_file")
+  echo "🔄 Running go-mod-upgrade in $dir ..."
+
+  cd "$dir"
+  go-mod-upgrade
+  go mod tidy
+  go test ./...
+  cd "$ROOT_DIR"
+done
+
+echo "✅ Ok."
