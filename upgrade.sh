39778e4785704737b670f849bbd0c68876e6694c
Ariel Piñeiro <ariel.pieiro@iskaypet.com>
2025-11-14 12:22:22 +0100

    fixes

--- upgrade.sh
+++ upgrade.sh
@@ -3,17 +3,21 @@ set -e
 
 ROOT_DIR=$(pwd)
 
+go work sync
+
 # Buscar todos los go.mod y recorrerlos
 for gomod_file in $(find . -name "go.mod"); do
   dir=$(dirname "$gomod_file")
   echo "🔄 Running go-mod-upgrade in $dir ..."
 
   cd "$dir"
-  go mod tidy
-  #go-mod-upgrade
+  go mod download
+  go-mod-upgrade
   go mod tidy
   go test ./...
   cd "$ROOT_DIR"
 done
 
 echo "✅ Ok."
+
+go work sync
